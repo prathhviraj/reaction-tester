5c89b55bb72c2d536b2291d8da3a4c36f0c289a0
prathhviraj <soniprithviraj15908@gmail.com>
2025-06-19 23:34:19 +0530

    New repo

---
+++ script.js
@@ -0,0 +1,50 @@
+// Generate a random time delay and position the box
+
+var start = new Date().getTime();
+
+function move(){
+    var top;
+    var left;
+    var wh;
+    top = Math.random()*300;
+    left = Math.random()*300;
+    wh = ((Math.random()*400 )+ 100);
+    document.getElementById("shape").style.left = left + "px";
+    document.getElementById("shape").style.top = top + "px";
+    document.getElementById("shape").style.width = wh + "px";
+    document.getElementById("shape").style.height = wh + "px";
+    
+    document.getElementById("shape").style.display = "block";
+    document.getElementById("shape").style.backgroundColor = getRandomColor();
+    start = new Date().getTime();
+
+    if (Math.random() > 0.5) {
+        shape.style.borderRadius = "50%";  // Circle
+    } 
+    else if (Math.random() > 0.7 >0.9){
+        shape.style.borderRadius = "30% 90%";
+    }
+     else {
+        shape.style.borderRadius = "0% ";   // Square
+    }
+}
+
+    function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+// Utility: Generate random hex color
+move()
+
+document.getElementById("shape").onclick = function(){
+    document.getElementById("shape").style.display= "none";
+    var end = new Date().getTime();
+    var timeTaken = (end - start)/1000
+    alert(timeTaken);
+    move()
+}
